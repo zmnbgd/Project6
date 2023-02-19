f7fdea41414b54c1ce47574edaf00d65cda0cdb3
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-20 00:47:34 +0100

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -58,7 +58,10 @@ class ViewController: UIViewController {
     //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label4]|", options: [], metrics: nil, views: viewsDicitionary))
     //        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[label5]|", options: [], metrics: nil, views: viewsDicitionary))
         }
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDicitionary))
+        
+        let metrics = ["labelHeight": 88]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)-(>=10)-|]", options: [], metrics: metrics, views: viewsDicitionary))
     }
 }
 
