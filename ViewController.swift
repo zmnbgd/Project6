3b4dae6993fa2ceeeed509c7b729cfb06a75c5fc
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2023-02-24 00:18:26 +0100

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -16,28 +16,32 @@ class ViewController: UITableViewController {
 
         let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         
-        if let url = URL(String: urlString) {
+        if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
-                //MARK: Ew are ok to parse that data
+                //MARK: We are ok to parse that data
+                parse(json: data)
             }
         }
     }
     
-    func parse(json: Date) {
+    func parse(json: Data) {
         let decoder = JSONDecoder()
         
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json)
-            petitions = jsonPetitons.result
-            
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =tableView?.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title"
-        cell.detailTextLabel?.text = "Subtitle"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 
